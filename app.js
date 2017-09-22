d1a1a547250aa3a6a1ea0f69a362a920e02d1ec0
ifelse monkey <ifelsemonkey@gmail.com>
2017-09-22 05:46:46 +0000

    serve via nodejs app

---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require("express");
+var app = express();
+
+app.use('/', express.static(__dirname + '/dist'));
+
+app.get('/env', function (req,res) {
+    var html = "<h1>Welcome</h1><hr><ul>";
+    for (x in process.env) {
+        var elem = `<li>${x} = ${process.env[x]}</li>`;
+        html += elem;
+    }
+    html += "</ul>";
+    res.send(html);
+});
+
+let port = process.env.PORT ? process.env.PORT : 6379;
+app.listen(port,function () {
+    console.log("listening %s",port);
+});
